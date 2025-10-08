adb5f0a04ca7de776054525ac208bc20476ecf3f
Joe Bally <joseph.bally@student.fairfield.edu>
2025-10-08 11:49:01 -0400

    New

--- Main/src/FileToList.java
+++ Main/src/FileToList.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class FileToList {
 
     // Reads a file and returns an ArrayList of lowercase words and punctuation
-    public ArrayList<String> readFileToList(String filePath) throws IOException {
+    public ArrayList<String> readFileToList(String path) throws IOException {
         ArrayList<String> list = new ArrayList<>();
 
-        File text = new File(filePath);
+        File text = new File(path);
         Scanner scan = new Scanner(text);
 
         while (scan.hasNextLine()) {
