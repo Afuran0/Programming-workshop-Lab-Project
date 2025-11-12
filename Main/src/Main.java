68197d00873b84de2f2c2a8866da11bdfd6b0c96
Joe Bally <joseph.bally@student.fairfield.edu>
2025-11-12 15:57:26 -0500

    New

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -11,7 +11,18 @@ public class Main{
         int choice = 0;
         int num;
 
-        ArrayList<Processor> articleList = new ArrayList<>();
+        ArrayList<Processor> topic1_articleList = new ArrayList<>();
+        ArrayList<Processor> topic2_articleList = new ArrayList<>();
+        ArrayList<Processor> topic3_articleList = new ArrayList<>();
+
+        ArrayList<ArrayList<Processor>> topicList = new ArrayList<>();
+        topicList.add(topic1_articleList);
+        topicList.add(topic2_articleList);
+        topicList.add(topic3_articleList);
+
+
+        ArrayList<ArticleComparison> acList = new ArrayList<>();
+
         ArrayList<String> articleFileNames = new ArrayList<>();
 
         //File names
@@ -26,38 +37,57 @@ public class Main{
 
         //Creates a list of Processor objects
         for (int i = 0; i < 3; i++){
-            articleList.add(new Processor(("Article" + i), articleFileNames.get(i)));
+            topic1_articleList.add(new Processor(("Article" + (i + 1)), articleFileNames.get(i)));
         }
 
-        //Objects and Inputs
-        ArticleComparison AC = new ArticleComparison(articleList);
+        //Creates Comparison Classes for each of the three topics and adds them to one AC List
+        ArticleComparison AC1 = new ArticleComparison(topic1_articleList);
+        ArticleComparison AC2 = new ArticleComparison(topic2_articleList);
+        ArticleComparison AC3 = new ArticleComparison(topic3_articleList);
+        acList.add(AC1);
+        acList.add(AC2);
+        acList.add(AC3);
+
 
         Scanner sc = new Scanner(System.in);
+        int topicChoice;
         while (running){
             System.out.println("\n\nWelcome to the File Manager");
+            System.out.println("What Topic Would you Like to Look At? (1, 2 or 3): ");
+            topicChoice = sc.nextInt();
+
             System.out.println("--------------------------------");
-            System.out.print("1. Article Statistics \n2. Word Frequencies \n3. Article Attitudes\n4. Exit\n");
+            System.out.print("1. Article Statistics \n2. Word Frequencies \n3. Richest Vocab\n4. Most Repeated\n5. Article Attitudes\n6. Exit");
             System.out.println("Enter Your choice: ");
 
             choice = sc.nextInt();
+
             switch (choice){
                 case 1:
                     System.out.println("Which Articles Stats Do You Want: ");
                     num = sc.nextInt();
-                    articleList.get(num - 1).statistics(true, false);
+                    topicList.get(topicChoice - 1).get(num - 1).statistics(true, false);
                     break;
                 case 2:
                     System.out.println("Which Articles Frequencies Do You Want: ");
                     num = sc.nextInt();
-                    articleList.get(num - 1).statistics(false, true);
+                    topicList.get(topicChoice - 1).get(num - 1).statistics(false, true);
                     break;
                 case 3:
-                    AC.articleAttitude();
+                    acList.get(topicChoice - 1).richestVocab();
                     break;
                 case 4:
+                    acList.get(topicChoice - 1).repeatedWord();
+                    break;
+                case 5:
+                    acList.get(topicChoice - 1).articleAttitude();
+                    break;
+                case 6:
                     System.out.println("Exiting.");
                     running = false;
+                    break;
                 default:
+                    System.out.println("Please Enter a Valid Option (1-6).");
                     break;
             }
         }
